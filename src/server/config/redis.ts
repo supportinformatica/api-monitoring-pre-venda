219de761e5abeff251b11b737007378ebb378a2d
iago fagundes <iago.fagundes@outlook.com>
2021-12-13 10:11:44 -0300

    fix: redis connection

--- src/server/config/redis.ts
+++ src/server/config/redis.ts
@@ -2,5 +2,8 @@ import Redis from 'ioredis';
 import * as Settings from '../settings';
 
 export const redisClient = new Redis(Settings.REDIS_URI, {
-  connectTimeout: Settings.REDIS_TIMEOUT
+  connectTimeout: Settings.REDIS_TIMEOUT,
+  tls: {
+    rejectUnauthorized: false
+  }
 });
