c25e14bb23879eeae6b26b9bc062a18fe6c771a1
iago fagundes <iago.fagundes@outlook.com>
2021-12-13 10:32:51 -0300

    fix: token service

--- src/modules/common/services/token-service.ts
+++ src/modules/common/services/token-service.ts
@@ -4,6 +4,7 @@ import * as Settings from '@src/server/settings';
 import { left, right } from '@src/shared/either';
 import { InvalidTokenError } from '../errors/invalid-token';
 import { IAuthorizedAdmin, TokenServiceDTO, VerifyTokenResponse } from './dtos/token-service';
+import { Injectable } from '@nestjs/common';
 
 interface VerifiedToken {
   sub: string;
@@ -11,16 +12,15 @@ interface VerifiedToken {
   exp: number;
 }
 
+@Injectable()
 export class TokenService implements TokenServiceDTO {
-  constructor(private readonly jwtService = JwtService) {}
-
   public verify(accessToken?: string): VerifyTokenResponse {
     if (!accessToken) return left(new InvalidTokenError(accessToken));
 
     const [_, token] = accessToken.split(' ');
 
     try {
-      const { sub } = this.jwtService.verify(token, Settings.AUTH_KEY_SECURITY) as VerifiedToken;
+      const { sub } = JwtService.verify(token, Settings.AUTH_KEY_SECURITY) as VerifiedToken;
 
       if (Number.isNaN(Number(sub))) return left(new InvalidTokenError(accessToken));
 
