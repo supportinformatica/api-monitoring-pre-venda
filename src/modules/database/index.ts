5acb9877782910fa34890a0f141ff345cb3ba450
iago fagundes <iago.fagundes@outlook.com>
2021-12-13 09:38:13 -0300

    fix: remove mongo connection

--- src/modules/database/index.ts
+++ src/modules/database/index.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { mongoConnection, mongoOptions } from './config/mongo-config';
 import { sqlServerConnection } from './config/mssql-config';
 
+// TODO: connect to mongo
+
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(sqlServerConnection),
-    MongooseModule.forRoot(mongoConnection, mongoOptions)
-  ]
+  imports: [TypeOrmModule.forRoot(sqlServerConnection)]
 })
 export class DatabaseModule {}
