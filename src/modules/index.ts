61cfaa662bea47e438c847ae5ad5140c890f5495
iago fagundes <iago.fagundes@outlook.com>
2021-12-13 12:45:00 -0300

    fix: start

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin';
 import { AppModule } from './app';
+import { CommonModule } from './common';
 import { DatabaseModule } from './database';
 
 @Module({
-  imports: [DatabaseModule, AdminModule, AppModule]
+  imports: [DatabaseModule, CommonModule, AdminModule, AppModule]
 })
 export class Modules {}
