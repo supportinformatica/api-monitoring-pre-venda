18a43acc2409e87255fcf38928b11aee5c9fcfd3
iago fagundes <iago.fagundes@outlook.com>
2021-12-21 11:07:28 -0300

    feat: add admin env

--- make.sh
+++ make.sh
@@ -4,25 +4,39 @@ read -p 'Set default environment (Y|N): ' results
 
 if [ "$results" == "y" ] || [ "$results" == "Y" ] 
   then 
+    echo "### Server config ###" >> .env
     echo "NODE_ENV=development" >> .env
     echo "HOST=0.0.0.0" >> .env
     echo "PORT=8080" >> .env
     echo >> .env
+    echo "### Auth config ###" >> .env
     echo "AUTH_KEY_SECURITY=secret" >> .env
     echo "AUTH_KEY_TOKEN_EXPIRES=300000" >> .env
     echo >> .env
-    echo "MSSQL_HOST=localhost" >> .env
-    echo "MSSQL_PORT=1433" >> .env
-    echo "MSSQL_NAME=root" >> .env
-    echo "MSSQL_USER=root" >> .env
-    echo "MSSQL_PASS=root" >> .env
+    echo "### SQL Server config (APP) ###" >> .env
+    echo "MSSQL_APP_CONNECTION_NAME=app" >> .env
+    echo "MSSQL_APP_HOST=localhost" >> .env
+    echo "MSSQL_APP_PORT=1433" >> .env
+    echo "MSSQL_APP_NAME=root" >> .env
+    echo "MSSQL_APP_USER=root" >> .env
+    echo "MSSQL_APP_PASS=root" >> .env
     echo >> .env
+    echo "### SQL Server config (ADMIN) ###" >> .env
+    echo "MSSQL_ADMIN_CONNECTION_NAME=admin" >> .env
+    echo "MSSQL_ADMIN_HOST=localhost" >> .env
+    echo "MSSQL_ADMIN_PORT=1433" >> .env
+    echo "MSSQL_ADMIN_NAME=root" >> .env
+    echo "MSSQL_ADMIN_USER=root" >> .env
+    echo "MSSQL_ADMIN_PASS=root" >> .env
+    echo >> .env
+    echo "### Mongo config ###" >> .env
     echo "MONGO_HOST=localhost" >> .env
     echo "MONGO_PORT=27017" >> .env
     echo "MONGO_NAME=test" >> .env
     echo "MONGO_USER=root" >> .env
     echo "MONGO_PASS=root" >> .env
     echo >> .env
+    echo "### Redis config ###" >> .env
     echo "REDIS_HOST=localhost" >> .env
     echo "REDIS_PORT=27017" >> .env
 
@@ -30,6 +44,8 @@ else
   echo ""
   echo "### Server config ###"
   echo ""
+
+  echo "### Server config ###" >> .env
 
   read -p 'Node environment (development): ' INPUT
     [ -z "$INPUT" ] && echo "NODE_ENV=development" >> .env || echo "NODE_ENV=$INPUT" >> .env
@@ -48,6 +64,8 @@ else
   echo "### Auth config ###"
   echo ""
 
+  echo "### Auth config ###" >> .env
+
   read -p 'Auth key security (secret): ' INPUT
     [ -z "$INPUT" ] && echo "AUTH_KEY_SECURITY=secret" >> .env || echo "AUTH_KEY_SECURITY=$INPUT" >> .env
 
@@ -58,23 +76,55 @@ else
   echo ""
   echo "----------------------------"
 
-  echo "### SQL Server config ###"
+  echo "### SQL Server config (APP) ###"
   echo ""
 
+  echo "### SQL Server config (APP) ###" >> .env
+
+  read -p 'Connection name (app): ' INPUT
+    [ -z "$INPUT" ] && echo "MSSQL_APP_CONNECTION_NAME=app" >> .env || echo "MSSQL_APP_CONNECTION_NAME=$INPUT" >> .env
+
   read -p 'SQL Server host (localhost): ' INPUT
-    [ -z "$INPUT" ] && echo "MSSQL_HOST=localhost" >> .env || echo "MSSQL_HOST=$INPUT" >> .env
+    [ -z "$INPUT" ] && echo "MSSQL_APP_HOST=localhost" >> .env || echo "MSSQL_APP_HOST=$INPUT" >> .env
 
   read -p 'SQL Server port (1433): ' INPUT
-    [ -z "$INPUT" ] && echo "MSSQL_PORT=1433" >> .env || echo "MSSQL_PORT=$INPUT" >> .env
+    [ -z "$INPUT" ] && echo "MSSQL_APP_PORT=1433" >> .env || echo "MSSQL_APP_PORT=$INPUT" >> .env
 
   read -p 'SQL Server database (root): ' INPUT
-    [ -z "$INPUT" ] && echo "MSSQL_NAME=root" >> .env || echo "MSSQL_NAME=$INPUT" >> .env
+    [ -z "$INPUT" ] && echo "MSSQL_APP_NAME=root" >> .env || echo "MSSQL_APP_NAME=$INPUT" >> .env
 
   read -p 'SQL Server user (root): ' INPUT
-    [ -z "$INPUT" ] && echo "MSSQL_USER=root" >> .env || echo "MSSQL_USER=$INPUT" >> .env
+    [ -z "$INPUT" ] && echo "MSSQL_APP_USER=root" >> .env || echo "MSSQL_APP_USER=$INPUT" >> .env
 
   read -sp 'SQL Server password (root): ' INPUT
-    [ -z "$INPUT" ] && echo "MSSQL_PASS=root" >> .env || echo "MSSQL_PASS=$INPUT" >> .env
+    [ -z "$INPUT" ] && echo "MSSQL_APP_PASS=root" >> .env || echo "MSSQL_APP_PASS=$INPUT" >> .env
+
+  echo >> .env
+  echo ""
+  echo "----------------------------"
+
+  echo "### SQL Server config (ADMIN) ###"
+  echo ""
+
+  echo "### SQL Server config (ADMIN) ###" >> .env
+
+  read -p 'Connection name (admin): ' INPUT
+    [ -z "$INPUT" ] && echo "MSSQL_ADMIN_CONNECTION_NAME=admin" >> .env || echo "MSSQL_ADMIN_CONNECTION_NAME=$INPUT" >> .env
+
+  read -p 'SQL Server host (localhost): ' INPUT
+    [ -z "$INPUT" ] && echo "MSSQL_ADMIN_HOST=localhost" >> .env || echo "MSSQL_ADMIN_HOST=$INPUT" >> .env
+
+  read -p 'SQL Server port (1433): ' INPUT
+    [ -z "$INPUT" ] && echo "MSSQL_ADMIN_PORT=1433" >> .env || echo "MSSQL_ADMIN_PORT=$INPUT" >> .env
+
+  read -p 'SQL Server database (root): ' INPUT
+    [ -z "$INPUT" ] && echo "MSSQL_ADMIN_NAME=root" >> .env || echo "MSSQL_ADMIN_NAME=$INPUT" >> .env
+
+  read -p 'SQL Server user (root): ' INPUT
+    [ -z "$INPUT" ] && echo "MSSQL_ADMIN_USER=root" >> .env || echo "MSSQL_ADMIN_USER=$INPUT" >> .env
+
+  read -sp 'SQL Server password (root): ' INPUT
+    [ -z "$INPUT" ] && echo "MSSQL_ADMIN_PASS=root" >> .env || echo "MSSQL_ADMIN_PASS=$INPUT" >> .env
 
   echo >> .env
   echo ""
@@ -82,6 +132,8 @@ else
 
   echo "### Mongo config ###"
   echo ""
+
+  echo "### Mongo config ###" >> .env
 
   read -p 'Mongo host (localhost): ' INPUT
     [ -z "$INPUT" ] && echo "MONGO_HOST=localhost" >> .env || echo "MONGO_HOST=$INPUT" >> .env
@@ -104,6 +156,8 @@ else
 
   echo "### Redis config ###"
   echo ""
+
+  echo "### Redis config ###" >> .env
 
   read -p 'Redis host (localhost): ' INPUT
     [ -z "$INPUT" ] && echo "REDIS_HOST=localhost" >> .env || echo "REDIS_HOST=$INPUT" >> .env
