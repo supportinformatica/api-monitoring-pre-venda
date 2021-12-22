bc6fa8495c82ecdefce4535682e5039174abb655
iago fagundes <iago.fagundes@outlook.com>
2021-12-22 08:48:35 -0300

    fix: verify time

--- src/modules/admin/last_update/jobs/index.ts
+++ src/modules/admin/last_update/jobs/index.ts
@@ -6,8 +6,9 @@ import { LastUpdateService } from '../services';
 export class LastUpdateJobs {
   constructor(private lastUpdateService: LastUpdateService) {}
 
-  @Cron('00 30 8-16 * * 1-5')
+  @Cron('00 */30 8-16 * * 1-5')
   public handleCron() {
+    console.log(`#### VERIFY LAST UPDATE (${new Date()})####`);
     this.lastUpdateService.verifyLastUpdate();
   }
 }
