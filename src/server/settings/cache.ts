101caba64fb8c270f85395ba078c3215b9434c29
iago fagundes <iago.fagundes@outlook.com>
2022-01-31 09:18:23 -0300

    fix: redis timeout

--- src/server/settings/cache.ts
+++ src/server/settings/cache.ts
@@ -4,11 +4,7 @@ const REDIS_PORT = process.env.REDIS_PORT || '6379';
 
 export const REDIS_URI = process.env.REDIS_TLS_URL || `redis://@${REDIS_HOST}:${REDIS_PORT}`;
 
-export const REDIS_TIMEOUT = 1000 * 10; // 10seg
-
-// TODO: UM MINUTO PARA CACHE
-
-// export const REDIS_EXPIRATION_TIME = 1000 * 60 * 60; // 1min
+export const REDIS_TIMEOUT = 1000 * 60 * 60; // 10seg
 
 export const REDIS_EXPIRATION_TIME = 1000 * 30;
 
